b8c20cca64ce2adde138972fa49b58d5407a4c9c
JOSUE RIVERA <josuerivera@JOSUEs-Air.hitronhub.home>
2019-11-06 12:20:58 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("connected");
+
+//toggles the class show-description to show or hide menu item description
+$('div').on('click', function() {
+    $(this).toggleClass('show-description');
+});
